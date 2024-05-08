910e3d085f9f2c1ec5e98fe661438fe1a5ec87b1
iajayparihar <ajayparihar876@gmail.com>
2024-05-08 15:34:10 +0530

    Done : custom signals & managemanet cmd

--- CBV_fb_clone/apps/register/models.py
+++ CBV_fb_clone/apps/register/models.py
@@ -1,15 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-gen = (("Female", 'Female'),
-        ("Male", 'Male'),
-        ('Trans','Trans'),
-        ("Custom", 'Custom'),)
+gen = (
+    ("Female", "Female"),
+    ("Male", "Male"),
+    ("Trans", "Trans"),
+    ("Custom", "Custom"),
+)
 
 class CustomUser(AbstractUser):
-    mobile = models.CharField( max_length=60)
-    DOB = models.DateField(auto_now_add=False,blank=True,null=True)
+    mobile = models.CharField(max_length=60)
+    DOB = models.DateField(auto_now_add=False, blank=True, null=True)
     gender = models.CharField(max_length=60, choices=gen)
-    
+
     def __str__(self):
         return self.username
+
+#===============Signal's===========================================
+from django.dispatch import receiver 
+from django.db.models.signals import post_save
+import datetime
+from django.contrib.auth.signals import user_logged_out,user_logged_in
+from django.db.backends.signals import connection_created
+
+@receiver(connection_created)
+def connection_created_handler(sender, connection, **kwargs):
+    current_time = datetime.datetime.now()
+    with open("logdata.txt", "a") as file:
+        file.write(str(current_time) + "\t" + "Database connection successfully." + "\n")
+
+@receiver(post_save, sender=CustomUser)
+def user_pre_save_api(sender, instance, created, **kwargs):
+    current_time = datetime.datetime.now()
+    if created:  # Only execute for newly created instances
+        with open("logdata.txt", "a") as file:
+            file.write(str(current_time) + "\t" + instance.username + " " + "User created successfully" + "\n")
+    else:
+        with open("logdata.txt", "a") as file:
+            file.write(str(current_time) + "\t" + instance.username + " " + "Some changes in Database." + "\n")
+
+@receiver(user_logged_in)
+def user_login(sender, request, **kwargs):
+    current_time = datetime.datetime.now()
+    with open("logdata.txt", "a") as file:
+        file.write(str(current_time) + "\t" + request.user.username + " " + "User login successfully." + "\n")
+
+@receiver(user_logged_out)
+def user_logout(sender, request, **kwargs):
+    current_time = datetime.datetime.now()
+    with open("logdata.txt", "a") as file:
+        file.write(str(current_time) + "\t" + request.user.username + " " + "User logout successfully." + "\n")
--- CBV_fb_clone/apps/register/views.py
+++ CBV_fb_clone/apps/register/views.py
@@ -18,7 +18,7 @@ def my_signal(sender,instance, **kwargs):
     message = kwargs.get("message")
     print(f"Received signal from {instance.username}: {message}")
 
-hello_signal.send(sender=None,instance=CustomUser.objects.all().first() ,message="Hello, world!")
+hello_signal.send(sender=None,instance=CustomUser.objects.all().first() ,message="Custom Signal form view's : Hello, u r logged in")
 #---------------------------------------------------------
 
 class register(generic.CreateView):
@@ -28,6 +28,7 @@ class register(generic.CreateView):
 
 class profile(generic.TemplateView):
     template_name='post/dashboard.html'
+    
 
     
 #     def logout_then_login(self,request, login_url='/accounts/login/'):
@@ -37,24 +38,24 @@ class profile(generic.TemplateView):
 #         super().dispatch(request)  # Call the parent class method to handle logout
 #         return redirect(resolve(login_url))
     
-class user_login(generic.View):
-    form_class = AuthenticationForm
-    template_name = 'registration/login.html'    
-    def get(self,request):
-        form = self.form_class
-        return render(request, self.template_name, {'form':form})
+# class user_login(generic.View):
+#     form_class = AuthenticationForm
+#     template_name = 'registration/login.html'    
+#     def get(self,request):
+#         form = self.form_class
+#         return render(request, self.template_name, {'form':form})
     
-    def post(self,request):
-        fm = AuthenticationForm(request=request,data = request.POST)
-        if fm.is_valid():
-            username = fm.cleaned_data.get('username')
-            password = fm.cleaned_data.get('password')
-            if user := authenticate(username = username, password = password):
-                login(request,user)
-                return redirect('Register:profile')
-        else:
-            fm = AuthenticationForm(request.POST)
-        return render(request,'registration/login.html',{'form':fm,'message':'try again!!!'})
+#     def post(self,request):
+#         fm = AuthenticationForm(request=request,data = request.POST)
+#         if fm.is_valid():
+#             username = fm.cleaned_data.get('username')
+#             password = fm.cleaned_data.get('password')
+#             if user := authenticate(username = username, password = password):
+#                 login(request,user)
+#                 return redirect('Register:profile')
+#         else:
+#             fm = AuthenticationForm(request.POST)
+#         return render(request,'registration/login.html',{'form':fm,'message':'try again!!!'})
     
 
 # class user_logout(generic.View):
